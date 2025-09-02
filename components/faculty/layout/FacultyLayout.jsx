d9c2a54f26bec3cbd5c93acabba9a00ae425ad07
ayeshamashiat <ayesha.mashiat101@gmail.com>
2025-09-02 08:35:19 +0600

    changed layout look

--- components/faculty/layout/FacultyLayout.jsx
+++ components/faculty/layout/FacultyLayout.jsx
@@ -6,7 +6,7 @@ export default function FacultyLayout({ children }) {
   return (
     <div className="flex max-h-screen">
       <FacultySidebar />
-      <div className="flex-1 bg-gray-50">{children}</div>
+      <div className="flex-1 bg-gray-50 overflow-y-auto">{children}</div>
     </div>
   );
 }
